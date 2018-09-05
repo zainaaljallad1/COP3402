7db19a5b0a79f780ad8c62fb410305a84a686921
zainaaljallad1 <zaina.aljallad@knights.ucf.edu>
2018-09-04 20:02:48 -0400

    initialization

---
+++ pMachine.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+BasePointer - BP 
+StackPointer - SP
+ProgramCounter - PC
+InstructionRegister - IR 
+ResgisterFile - RF // 16 registers (0-15) 
+
+PM/0 has 24 instructions 
+FORMAT: OP R L M 
+
+
+int main (void) {
+
+	
+
+} 
